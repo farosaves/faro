4ef6aa1eaf6711e61090344341376f8d81c9f1fa
mefuru <mehul.mandania@gmail.com>
2023-12-29 14:47:40 +0000

    Add tw and daisyUI

--- supabase-sveltekit/src/routes/+page.svelte
+++ supabase-sveltekit/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../styles.css';
+  </script>'
+
+<div class="p-8 bg-base-200 text-base-content">
+    <h1 class="mb-4 text-4xl font-bold ">SvelteKit</h1>
+    <p class="mb-4">
+      Visit 
+      <a href="https://kit.svelte.dev" class="link link-primary">kit.svelte.dev</a> 
+      to read the documentation
+    </p>
+  
+    <!-- Additional DaisyUI Components -->
+    <button class="btn btn-primary">Primary Button</button>
+    <button class="mt-2 btn btn-secondary">Secondary Button</button>
+  
+    <div class="divider"></div> <!-- Divider Component -->
+  
+    <div class="mt-4 shadow-xl card w-96 bg-base-100">
+      <div class="card-body">
+        <h2 class="card-title">DaisyUI Card</h2>
+        <p>This is a card component from DaisyUI.</p>
+        <div class="justify-end card-actions">
+          <button class="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
