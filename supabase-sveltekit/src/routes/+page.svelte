06e3b1cffff1b88e1aed69753732a2f58fb4f94d
mefuru <mehul.mandania@gmail.com>
2023-12-29 15:44:59 +0000

    Add user auth hooks, implement magic link sign in

--- supabase-sveltekit/src/routes/+page.svelte
+++ supabase-sveltekit/src/routes/+page.svelte
@@ -1,29 +1,24 @@
 <script>
     import '../styles.css';
-  </script>'
+    import { Auth } from '@supabase/auth-ui-svelte'
+	import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-<div class="p-8 bg-base-200 text-base-content">
-    <h1 class="mb-4 text-4xl font-bold ">SvelteKit</h1>
-    <p class="mb-4">
-      Visit 
-      <a href="https://kit.svelte.dev" class="link link-primary">kit.svelte.dev</a> 
-      to read the documentation
-    </p>
-  
-    <!-- Additional DaisyUI Components -->
-    <button class="btn btn-primary">Primary Button</button>
-    <button class="mt-2 btn btn-secondary">Secondary Button</button>
-  
-    <div class="divider"></div> <!-- Divider Component -->
-  
-    <div class="mt-4 shadow-xl card w-96 bg-base-100">
-      <div class="card-body">
-        <h2 class="card-title">DaisyUI Card</h2>
-        <p>This is a card component from DaisyUI.</p>
-        <div class="justify-end card-actions">
-          <button class="btn btn-primary">Get Started</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  
+	export let data
+  </script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="flex row flex-center">
+	<div class="col-6 form-widget">
+		<Auth
+			supabaseClient={data.supabase}
+			view="magic_link"
+			redirectTo={`${data.url}/auth/callback`}
+			showLinks={false}
+			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
+		/>
+	</div>
+</div>
+
