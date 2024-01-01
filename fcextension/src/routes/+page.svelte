627ec9b6499f0ab3477cbe6d07ccb2dc068750d9
asjir <pa.paradysz@gmail.com>
2024-01-01 20:02:18 +0100

    svelte extension sanity check

--- fcextension/src/routes/+page.svelte
+++ fcextension/src/routes/+page.svelte
@@ -1,5 +1,12 @@
+<script>
+  let cnt = 1;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<button class="btn btn-primary">Hello daisyUI</button>
+<button class="btn btn-primary" on:click={() => (cnt += 1)}
+  >Hello daisyUI</button
+>
+<p>{cnt}</p>
