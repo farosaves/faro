5e6b95469a96e7e0b9eb3307352aaeda8435d351
asjir <pa.paradysz@gmail.com>
2024-01-04 13:17:54 +0100

    factoring out snippet component

--- fcextension/src/routes/sidepanel/+page.svelte
+++ fcextension/src/routes/sidepanel/+page.svelte
@@ -1,130 +1,17 @@
 <script>
-  const DOMAIN = "http://localhost:5173";
-  let n_cards = 2;
-  let qas = [
-    ["Kto ty jestes? ", "Polak maly"],
-    ["Jaki znak twoj?", "Orzel bialy"],
-  ];
-  let qas_accepted = [false, false];
-  let qas_rejected = [false, false];
-  let card_ids = [null, null];
-  let website_title = "Chick flick";
-  let link = "https://en.wikipedia.org/wiki/Chick_flick";
-  let text =
-    "Women are typically portrayed in chick flicks as sassy, noble victims, or klutzy twentysomethings. Romantic comedies (rom-coms) are often also chick flicks. However, rom-coms are typically respected more than chick flicks because they are designed to appeal to men and women.";
-
   export let data;
-  let session = data.session;
-  let supabase = data.supabase;
-  let snippet_id = undefined;
-
-  async function callapi() {
-    const response = await fetch(`${DOMAIN}/api/make-flashcard`, {
-      method: "POST",
-      body: JSON.stringify({ n_cards, text, website_title, link, session }),
-      headers: {
-        "content-type": "application/json",
-      },
-    });
-    let ret = await response.json();
-    qas = ret.qas;
-    snippet_id = ret.snippet_id;
-    card_ids = ret.card_ids;
-    qas_accepted = card_ids.map((x) => false);
-    qas_rejected = card_ids.map((x) => false);
-    console.log(qas);
-  }
-  function reject(n) {
-    return () => {
-      qas_rejected[n] = true;
-      qas_accepted[n] = false;
-    };
-  }
-  function accept(n) {
-    return async () => {
-      const { error } = await supabase
-        .from("cards")
-        .update({ is_active: true })
-        .eq("id", card_ids[n]);
-      console.log(error);
-      qas_rejected[n] = false;
-      qas_accepted[n] = true;
-    };
-  }
-  function accept_reject_undo(n) {
-    return async () => {
-      const { error } = await supabase
-        .from("cards")
-        .update({ is_active: false })
-        .eq("id", card_ids[n]);
-      console.log(error);
-      qas_rejected[n] = false;
-      qas_accepted[n] = false;
-    };
-  }
+  import Snippet from "$lib/Snippet.svelte";
 </script>
 
-<!-- <h1>All sites sidepanel extension</h1>
-<p>This side panel is enabled on all sites</p>
-<p class="text-2xl">tailwind test ( works fine)</p>
-<button class="btn btn-primary">daisy test</button> -->
-
-{(data.session?.user.email, snippet_id)}
-{card_ids.join(" ")}
-<div class="col-6 form-widget">
-  <input class="text-xl" bind:value={website_title} />
-  <input class="resize" bind:value={text} />
-
-  <div class="overflow-x-auto">
-    <table class="table">
-      <!-- head -->
-      <thead>
-        <tr>
-          <th></th>
-          <th>Question</th>
-          <th>Answer</th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- row 1 -->
-        {#each qas.entries() as [index, [question, answer]]}
-          <tr>
-            <th>{index + 1}</th>
-            <td>{question}</td>
-            <td>{answer}</td>
-            <td class="flex flex-col">
-              {#if !qas_accepted[index] && !qas_rejected[index]}
-                <button
-                  class="btn w-full"
-                  style="color:green"
-                  on:click={accept(index)}
-                >
-                  Accept</button
-                >
-                <button
-                  class="btn w-full"
-                  style="color:red"
-                  on:click={reject(index)}
-                >
-                  Reject</button
-                >
-              {:else}
-                <button
-                  class="btn w-full"
-                  style="color:{qas_accepted[index] ? 'green' : 'red'}"
-                  on:click={accept_reject_undo(index)}
-                >
-                  {qas_accepted[index] ? "Accepted" : "Rejected"}<br
-                  />(undo)</button
-                >
-              {/if}
-            </td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
-
-  <!-- <span class="text-center">{res}</span> -->
-  <button on:click={callapi}>Replicate test</button>
+<div class="max-w-xs mx-auto">
+  <Snippet {data} />
+  <span
+    class="block p-4 my-2 bg-green-500 text-white hover:bg-green-600 rounded-md"
+    >Clickable Text 2</span
+  >
+  <span
+    class="block p-4 my-2 bg-yellow-500 text-white hover:bg-yellow-600 rounded-md"
+    >Clickable Text 3</span
+  >
+  <div class="h-8"></div>
 </div>
