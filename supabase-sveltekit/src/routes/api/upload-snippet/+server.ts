1e0bde74231ee6e21c2ae0acb03e7e874a97e78f
Pawel Paradysz <pa.paradysz@gmail.com>
2024-01-14 16:37:25 +0100

    db refactor + formatting of notes

--- supabase-sveltekit/src/routes/api/upload-snippet/+server.ts
+++ supabase-sveltekit/src/routes/api/upload-snippet/+server.ts
@@ -3,20 +3,37 @@ import { logIfError } from '$lib/util.js';
 import { error, json } from '@sveltejs/kit';
 import { makeQuote } from './fun';
 
+let hostname = (s: string) => new URL(s).hostname;
 
 export async function POST({ request, locals }) {
 	const { selectedText, contextText, website_url, website_title } = await request.json();
 	console.log('uploaded:', { selectedText, contextText });
 	const supabase: SupabaseClient = locals.supabase; // here this loads defined tables properly
 	// console.log((await locals.getSession())?.user.email)
-	const quote = makeQuote(selectedText,contextText);
-	if (contextText.split(" ").length < 2) return json({});
+	const quote = makeQuote(selectedText, contextText);
+	if (contextText.split(' ').length < 2 || selectedText.length < 2) return json({});
 	const { data } = await supabase
-		.from("notes")
-		.insert({ quote, origin_website: website_url, website_title })
-		.select()
-		.maybeSingle()
+		.from('sources')
+		.select('id')
+		.eq('domain', hostname(website_url))
+		.eq('title', website_title)
+		.maybeSingle();
+	let source_id;
+	if (data) source_id = data.id;
+	else {
+		const { data } = await supabase
+			.from('sources')
+			.insert({ domain: hostname(website_url), url: website_url, title: website_title })
+			.select('id')
+			.maybeSingle()
+			.then(logIfError);
+		source_id = data!.id;
+	}
+
+	await supabase
+		.from('notes')
+		.insert({ quote, source_id, highlights: [selectedText] })
 		.then(logIfError);
 
-	return json({data});
+	return json({});
 }
