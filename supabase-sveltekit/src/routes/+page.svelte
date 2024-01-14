df09e9c5cd8bbe2969057bf5ba66c94aa69044e5
Pawel Paradysz <pa.paradysz@gmail.com>
2024-01-14 09:19:13 +0100

    password auth

--- supabase-sveltekit/src/routes/+page.svelte
+++ supabase-sveltekit/src/routes/+page.svelte
@@ -3,6 +3,10 @@
 	import { Auth } from '@supabase/auth-ui-svelte';
 	import '../styles.css';
 	export let data;
+	import Radio from '$lib/Radio.svelte';
+	// @ts-ignore
+	let view = 'magic_link';
+	let view_options = ['sign_in', 'sign_up', 'magic_link', 'forgotten_password'];
 </script>
 
 <svelte:head>
@@ -13,10 +17,11 @@
 	<div class="col-6 form-widget">
 		<Auth
 			supabaseClient={data.supabase}
-			view="magic_link"
 			redirectTo={`${data.url}/auth/callback`}
 			showLinks={false}
+			{view}
 			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
 		/>
+		<Radio options={view_options} bind:userSelected={view} />
 	</div>
 </div>
