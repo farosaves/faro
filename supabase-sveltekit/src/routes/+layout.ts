3e64ce6b8bca47227a0dc9d857f515ad3bba1a97
Pawel Paradysz <pa.paradysz@gmail.com>
2024-01-17 17:17:52 +0100

    removed commented logging

--- supabase-sveltekit/src/routes/+layout.ts
+++ supabase-sveltekit/src/routes/+layout.ts
@@ -21,33 +21,32 @@
 // 	return { supabase, session };
 // };
 
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
-import type { Database } from '$lib/dbtypes'
-import type { LayoutLoad } from './$types'
-import { createBrowserClient, isBrowser, parse } from '@supabase/ssr'
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import type { Database } from '$lib/dbtypes';
+import type { LayoutLoad } from './$types';
+import { createBrowserClient, isBrowser, parse } from '@supabase/ssr';
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
-  depends('supabase:auth')
-  const supabase = createBrowserClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-    global: {
-      fetch,
-    },
-    cookies: {
-      get(key) {
-        if (!isBrowser()) {
-          return JSON.stringify(data.session)
-        }
+	depends('supabase:auth');
+	const supabase = createBrowserClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+		global: {
+			fetch
+		},
+		cookies: {
+			get(key) {
+				if (!isBrowser()) {
+					return JSON.stringify(data.session);
+				}
 
-        const cookie = parse(document.cookie)
-        return cookie[key]
-      },
-    },
-  })
-  // if (isBrowser()) {console.log(document.cookie);}
+				const cookie = parse(document.cookie);
+				return cookie[key];
+			}
+		}
+	});
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
-  // console.log(session)
-  return { supabase, session }
-}
+	const {
+		data: { session }
+	} = await supabase.auth.getSession();
+	// console.log(session)
+	return { supabase, session };
+};
--- supabase-sveltekit/src/routes/api/my-email/+server.js
+++ supabase-sveltekit/src/routes/api/my-email/+server.js
@@ -1,8 +1,5 @@
 import { error, json } from '@sveltejs/kit';
 export async function POST({ request, locals }) {
-	// console.log('rc\n');
-	// console.log(request.headers.get('cookie'));
-	// console.log(await locals.getSession());
 	let sess = await locals.getSession();
 	if (sess) {
 		const { user, access_token, refresh_token } = sess;
