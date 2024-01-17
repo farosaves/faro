9c1eb54d21cfcbe0eae7d8ac03609e1b8d55fc20
Pawel Paradysz <pa.paradysz@gmail.com>
2024-01-17 17:48:11 +0100

    trpc

--- supabase-sveltekit/src/hooks.server.ts
+++ supabase-sveltekit/src/hooks.server.ts
@@ -1,7 +1,11 @@
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import type { Database } from '$lib/dbtypes';
+import { createContext } from '$lib/trpc/context';
+import { router } from '$lib/trpc/router';
 import { createServerClient } from '@supabase/ssr';
 import type { Handle } from '@sveltejs/kit';
+import { type Dict } from '@trpc/server';
+import { resolveHTTPResponse } from '@trpc/server/http';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.supabase = createServerClient<Database>(
@@ -32,14 +36,44 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return session;
 	};
 
-	const response =
-		event.request.method === 'OPTIONS'
-			? await resolve(event, {
-					filterSerializedResponseHeaders(name) {
-						return name === 'content-range';
-					}
-				})
-			: new Response(JSON.stringify({}), { status: 200 });
+	let response;
+	const url = '/trpc';
+	if (event.request.method === 'OPTIONS')
+		response = new Response(JSON.stringify({}), { status: 200 });
+	else if (event.url.pathname.startsWith(url + '/')) {
+		const request = event.request as Request & {
+			headers: Dict<string | string[]>;
+		};
+
+		const req = {
+			method: request.method,
+			headers: request.headers,
+			query: event.url.searchParams,
+			body: await request.text()
+		};
+
+		const httpResponse = await resolveHTTPResponse({
+			router,
+			req,
+			path: event.url.pathname.substring(url.length + 1),
+			createContext: async () => createContext?.(event)
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		});
+
+		const { status, headers, body } = httpResponse as {
+			status: number;
+			headers: Record<string, string>;
+			body: string;
+		};
+
+		response = new Response(body, { status, headers });
+	} else
+		response = await resolve(event, {
+			filterSerializedResponseHeaders(name) {
+				return name === 'content-range';
+			}
+		});
+
 	// console.log(event.request.headers.get("origin"))  e.g. chrome-extension://aomnlngcbnepejemfdjlllcmfhdppkio or localhost:5174
 	const origin = event.request.headers.get('origin');
 	// here if we want access control we can check origin programatically
