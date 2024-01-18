25bd612c01ca1496f373e47271190327ddd78a5e
Pawel Paradysz <pa.paradysz@gmail.com>
2024-01-18 16:13:15 +0100

    fixed a bit..

--- supabase-sveltekit/src/routes/api/upload-snippet/fun.ts
+++ supabase-sveltekit/src/routes/api/upload-snippet/fun.ts
@@ -4,28 +4,32 @@ import { split } from 'sentence-splitter';
 
 let tooLong = (s: string) => split(s).filter((s) => s.raw.length > 4).length > 10;
 let longEnough = (s: string) => split(s).filter((s) => s.raw.length > 4).length > 1;
-let isValid = (s: string) => longEnough(s) && !tooLong(s);
-let firstValid = flow(A.findLast(isValid), O.toUndefined); // TODO: this can make problems
-
-export function makeQuote(selectedText: string, contextText: string) {
-	if (isValid(selectedText)) {
-		return selectedText;
-	}
-	console.log(contextText, selectedText);
-	const regex_cit = /\[\d{1,2}\]/u;
-	const regex_post = /^([^\.!\?]|\.\d|\d\.|\.[^\.]\.|\w\.\w)+/u; // from the start
-	const regex_pre = /([^\.\?]|\.\d|\d\.|\.[^\.]\.|\w\.\w)+$/u; // from the end
-	let [pre, ...posts] = contextText.replace(regex_cit, '').split(selectedText);
-	let post = posts.join(selectedText);
-	let f = (v: RegExpMatchArray | null) => (v && v[0]) || '';
-	return (f(pre.match(regex_pre)) + selectedText + f(post.match(regex_post))).trim() + '.';
-	// return contextText.slice(pre, contextText.length-post)
-}
+let isValidQuote = (s: string) => longEnough(s) && !tooLong(s);
+let firstValid = flow(A.findLast(isValidQuote), O.toUndefined); // TODO: this can make problems
+let iou = (s1: string) => (s2: string) =>
+	new Set(...s1.split(' ').filter((x) => new Set(...s2.split(' ')).has(x))).size /
+	new Set([...s1.split(' '), ...s2.split(' ')]).size;
+// let a = Set(s1), b= Set(s2); length(a ∪ b)/length(a ∩ b) end in julia for comparison lol
 
 // Quote Context Highlights
 export function makeQCH(selectedText: string, contextTexts: string[]) {
+	const regex_cit = /\[\d{1,2}\]/gu;
+	selectedText = selectedText.replaceAll('&quot;', '"').replaceAll(regex_cit, '');
 	const context = firstValid(contextTexts)!;
 	const highlights = !longEnough(selectedText) ? [selectedText] : [];
-	const quote = makeQuote(selectedText, context);
+	let quote: string;
+	if (longEnough(selectedText)) quote = selectedText;
+	else {
+		let potential_quotes = split(context.replaceAll(regex_cit, ''))
+			.map((x) => x.raw)
+			.filter((x) => x.includes(selectedText))
+			.toSorted(iou(contextTexts[0]));
+		potential_quotes = // this is a hacky fallback..
+			potential_quotes ||
+			split(context.replaceAll(regex_cit, ''))
+				.map((x) => x.raw)
+				.toSorted(iou(selectedText));
+		quote = potential_quotes.findLast(() => true)!;
+	}
 	return { quote, highlights, context };
 }
