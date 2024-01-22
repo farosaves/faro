9bd5722382f4f0bfef2559551fca909e06804f8c
asjir <pa.paradysz@gmail.com>
2024-01-22 17:08:49 -0500

    looking around modifying text on page

--- fcextension/static/content.js
+++ fcextension/static/content.js
@@ -1,6 +1,17 @@
 import 'chrome';
 console.log('hello');
 
+function wrapSelectedText(uuid) {
+	var selection = window.getSelection().getRangeAt(0);
+	var selectedText = selection.extractContents();
+	var span = document.createElement('u');
+	span.tabIndex = -1
+	span.appendChild(selectedText);
+	span.id = uuid
+	selection.insertNode(span);
+	span.focus()
+}
+
 // now we also need node text - to make sure that if highlight was from a different node in the paragraph we capture the correct one
 let node2context = (node) =>
 	node.textContent.split(/[\.\?!]/u).filter((s) => s.length > 4).length > 1 // at least 2 sentences
@@ -20,6 +31,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		const nodeText = window.getSelection().anchorNode.textContent;
 		console.log({ selectedText, nodeText });
 		const contextTexts = walkup(window.getSelection().anchorNode);
+		wrapSelectedText("hehehe")
 		chrome.runtime.sendMessage({
 			action: 'uploadText',
 			selectedText,
