9875992d2be0bdbf4c7f937f61f13be43a9ccff3
asjir <pa.paradysz@gmail.com>
2024-01-24 18:08:19 -0500

    .

--- supabase-sveltekit/src/lib/utils.ts
+++ supabase-sveltekit/src/lib/utils.ts
@@ -6,10 +6,10 @@ export function ts(card: Card) {
 	return { ...card, due: card.due.toString(), last_review: card.last_review.toString() };
 }
 
-function groupBy<T>(f: (a: T) => number) {
-	return (arr: T[]) => {
-		let keys = A.uniq(N.Eq)(A.map(f)(arr));
-        A.map((key: number) => A.filter((v: T) => f(v)==key))(keys)
-	};
-}
+// function groupBy<T>(f: (a: T) => number) {
+// 	return (arr: T[]) => {
+// 		let keys = A.uniq(N.Eq)(A.map(f)(arr));
+//         A.map((key: number) => A.filter((v: T) => f(v)==key))(keys)
+// 	};
+// }
 // let filter = (f) => A.filter(f)
