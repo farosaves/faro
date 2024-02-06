c525c7142ef2a704fe59b6997540fcb657349d0b
asjir <pa.paradysz@gmail.com>
2024-02-06 11:01:31 -0500

    .

--- supabase-sveltekit/src/lib/shared/note-sync.ts
+++ supabase-sveltekit/src/lib/shared/note-sync.ts
@@ -4,7 +4,7 @@ import type { Notes } from '../dbtypes';
 import type { Notess, SupabaseClient } from './first';
 import { derived, get, type Writable } from 'svelte/store';
 import { delete_by_id, getNotes, logIfError } from './utils';
-import { option as O, array as A, record as R } from 'fp-ts';
+import { option as O, array as A, record as R, task as T } from 'fp-ts';
 import { groupBy } from 'fp-ts/lib/NonEmptyArray';
 import { pipe } from 'fp-ts/lib/function';
 
@@ -24,6 +24,10 @@ export class NoteSync {
 	panel(id: number) {
 		return derived(this.notestore, (v) => v[id]);
 	}
+	alltags = () =>
+		derived(this.notestore, (v) =>
+			Object.entries(v).flatMap(([_, notess]) => notess.flatMap((n) => n.tags || []))
+		);
 
 	async update_one_page(id: number) {
 		if (!this.user_id) {
@@ -61,7 +65,7 @@ export class NoteSync {
 	get_groups() {
 		return derived(this.notestore, (kvs) =>
 			pipe(
-				Object.entries(kvs),  // @ts-ignore
+				Object.entries(kvs), // @ts-ignore
 				A.map(([k, v]) => [v[0].sources.title, v]),
 				R.fromEntries<Notess>,
 				R.toArray<string, Notess>
@@ -69,16 +73,45 @@ export class NoteSync {
 		);
 	}
 
-	deleteit = (n: Notes) => async () => {
-		let { error } = await this.sb.from('notes').delete().eq('id', n.id).then(logIfError);
-		if (error) return;
+	addit = async (n: Notes) => {
+		const foo = 'asdf' // string
+		const bar = T.of(foo) // T.Task<string>
+		const m = async (a: number) => a+a
+		const barr = T.of(m)
+		let s = (await barr())(1)
+
+		const cache = get(this.notestore)[n.source_id];
+		const title = cache
+			? cache[0].sources.title
+			: (await this.sb.from('sources').select().eq('id', n.source_id).maybeSingle()).data?.title ||
+				'missing Title';
+
+		this.notestore.update((s) => {
+			s[n.source_id] = [...s[n.source_id], { ...n, sources: { title } }];
+			return s;
+		});
+		this.sb.from('notes').insert(n).then(logIfError).then(this._restoreIE(n, cache));
+	};
+
+	deleteit = (n: Notes) => {
+		const cache = get(this.notestore)[n.source_id];
+
 		this.notestore.update((s) => {
 			s[n.source_id] = delete_by_id(n.id)(s[n.source_id]);
 			return s;
 		});
+		this.sb.from('notes').delete().eq('id', n.id).then(logIfError).then(this._restoreIE(n, cache));
 	};
+	// @ts-ignore
+	_restoreIE = (n: Notes, cache: Notess) => (r) =>
+		r.error &&
+		this.notestore.update((s) => {
+			s[n.source_id] = cache;
+			return s;
+		});
 
 	tagUpdate = (note: Notes) => (tag: string, tags: string[]) => {
+		console.log(note.highlights);
 		this.notestore.update((n) => {
 			n[note.source_id].filter((_note) => _note.id == note.id)[0].tags = tags;
 			return n;
