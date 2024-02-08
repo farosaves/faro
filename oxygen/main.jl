3765ba94191bfa98fcf30ebfb90ab5ba5d1ca31d
asjir <pa.paradysz@gmail.com>
2024-02-08 22:46:43 +0100

    .

--- oxygen/main.jl
+++ oxygen/main.jl
@@ -34,7 +34,7 @@ end
 
 
 
-run(; port=2227, async=true) = (!isdefined(Main, :s) || !isopen(Main.s)) && (Main.s = serve(middleware=[CorsHandler, ReviseHandler]; port, async, catch_errors=false))
+run(; port=13723, async=true) = (!isdefined(Main, :s) || !isopen(Main.s)) && (Main.s = serve(middleware=[CorsHandler, ReviseHandler]; port, async, catch_errors=false))
 run()
 
 # bin = parse.(UInt8, split(saved[].html, ","))
