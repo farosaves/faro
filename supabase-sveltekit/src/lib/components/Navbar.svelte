99f05c7e06c118810d08f5d43f30d333bda278bf
asjir <pa.paradysz@gmail.com>
2024-02-18 07:53:04 -0500

    theme

--- supabase-sveltekit/src/lib/components/Navbar.svelte
+++ supabase-sveltekit/src/lib/components/Navbar.svelte
@@ -1,3 +1,21 @@
+<script lang="ts">
+  const themes = ["light", "dark", "retro", "cyberpunk", "aqua", "night"];
+  // import { writable } from "svelte/store";
+  // let selectedTheme = "light";
+  // document.
+  import { onMount } from "svelte";
+  // onMount(() =>
+  //   document.documentElement.setAttribute("data-theme", selectedTheme),
+  // );
+  import { themeChange } from "theme-change";
+
+  // NOTE: the element that is using one of the theme attributes must be in the DOM on mount
+  onMount(() => {
+    themeChange(false);
+    // 👆 false parameter is required for svelte
+  });
+</script>
+
 <div class="navbar">
   <div class="navbar-start">
     <a class="btn btn-ghost text-xl" href="/">Faros</a>
@@ -10,6 +28,37 @@
     </ul>
   </div>
   <div class="navbar-end">
-    <!-- <a class="btn">Button</a> -->
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn m-1">Theme</div>
+      <div class="dropdown-content join join-vertical">
+        {#each themes as value}
+          <button
+            class="btn join-item"
+            data-set-theme={value}
+            data-act-class="ACTIVECLASS"
+            >{value.replace(/\b\w/g, (s) => s.toUpperCase())}</button>
+
+          <!-- <input
+            type="radio"
+            name="theme-buttons"
+            class="btn join-item"
+            bind:group={selectedTheme}
+            aria-label={value.replace(/\b\w/g, (s) => s.toUpperCase())}
+            {value} /> -->
+        {/each}
+      </div>
+    </div>
+
+    <!-- <div class="dropdown mb-72">
+      <div tabindex="0" role="button" class="btn m-1">
+        Theme
+        <svg width="12px" height="12px" class="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
+      </div>
+      <ul tabindex="0" class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+        {#each themes as value}
+        <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="{value.toUpperCase()}" {value}/></li>
+        {/each}
+      </ul>
+    </div> -->
   </div>
 </div>
