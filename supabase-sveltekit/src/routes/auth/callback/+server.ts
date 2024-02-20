bd7a4b00d4beaa9af392a8cfc39dd3549e707bdc
asjir <pa.paradysz@gmail.com>
2024-02-20 17:10:25 -0500

    reset password

--- supabase-sveltekit/src/routes/auth/callback/+server.ts
+++ supabase-sveltekit/src/routes/auth/callback/+server.ts
@@ -1,11 +1,27 @@
-import { redirect } from "@sveltejs/kit";
+// src/routes/auth/callback/+server.ts
+import { redirect } from "@sveltejs/kit"
+import { isAuthApiError } from "@supabase/supabase-js"
 
 export const GET = async ({ url, locals: { supabase } }) => {
-  const code = url.searchParams.get("code");
-
+  const code = url.searchParams.get("code")
   if (code) {
-    await supabase.auth.exchangeCodeForSession(code);
+    try {
+      await supabase.auth.exchangeCodeForSession(code)
+    } catch (error) {
+      // If you open in another browser, need to redirect to login.
+      // Should not display error
+      if (isAuthApiError(error)) {
+        throw redirect(303, "/login?verified=true")
+      } else {
+        throw error
+      }
+    }
   }
 
-  throw redirect(303, "/account");
-};
+  const next = url.searchParams.get("next")
+  if (next) {
+    throw redirect(303, next)
+  }
+
+  throw redirect(303, "/account")
+}
--- supabase-sveltekit/src/routes/notes/queue/util.ts
+++ supabase-sveltekit/src/routes/notes/queue/util.ts
@@ -3,7 +3,6 @@ import { flow, pipe } from "fp-ts/lib/function.js";
 import { startOfDay, parse, format } from "date-fns";
 import * as fd from "date-fns/fp";
 const fstr = "yyyy-MM-dd"
-type Date_ = [number, number, number];
 export const day = (hr = 5) =>
   flow(fd.subHours(hr), fd.format(fstr));
 export const fromDay =
