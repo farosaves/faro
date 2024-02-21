5e0182f63e39a0427c8ab1cf276806fba03e0c22
asjir <pa.paradysz@gmail.com>
2024-02-21 10:41:46 -0500

    more improvements

--- fcextension/src/lib/components/NotePanel.svelte
+++ fcextension/src/lib/components/NotePanel.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-	import type { NoteSync } from '../shared/note-sync';
-	export let note_sync: NoteSync;
-	let note_store = note_sync.notestore;
-	export let source_id: number;
-	import Note from '$lib/components/Note.svelte';
-	if (!(source_id in $note_store)) $note_store[source_id] = [];
-	let showing_contents = $note_store[source_id].map((_) => false);
-	let close_all_notes = () => {
-		showing_contents = showing_contents.map((_) => false);
-	};
+  import type { NoteSync } from "../shared/note-sync";
+  export let note_sync: NoteSync;
+  let note_store = note_sync.notestore;
+  export let source_id: number;
+  import Note from "$lib/components/Note.svelte";
+  if (!(source_id in $note_store)) $note_store[source_id] = [];
+  let showing_contents = $note_store[source_id].map((_) => false);
+  let close_all_notes = () => {
+    showing_contents = showing_contents.map((_) => false);
+  };
 </script>
 
 {#each $note_store[source_id] as note_data, i}
-	<Note {note_data} bind:showing_content={showing_contents[i]} {close_all_notes} {note_sync} />
-{:else}
-	If you just installed the extension, you need to reload the page.
+  <Note
+    {note_data}
+    bind:showing_content={showing_contents[i]}
+    {close_all_notes}
+    {note_sync} />
+
+  <!-- {:else}
+	If you just installed the extension, you need to reload the page. -->
 {/each}
