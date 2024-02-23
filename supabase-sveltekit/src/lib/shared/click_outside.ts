151172164a55575a44c6415e1974c2598e7b14a1
asjir <pa.paradysz@gmail.com>
2024-02-23 17:33:39 -0500

    ts expect error

--- supabase-sveltekit/src/lib/shared/click_outside.ts
+++ supabase-sveltekit/src/lib/shared/click_outside.ts
@@ -1,6 +1,8 @@
 export function clickOutside(node: Node) {
   const handleClick = (event: Event) => {
+    // @ts-expect-error
     if (node && !node.contains(event.target) && !event.defaultPrevented) {
+      // @ts-expect-error
       node.dispatchEvent(new CustomEvent("click_outside", node))
     }
   }
