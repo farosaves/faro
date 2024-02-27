23f389184451c0df9e6c9086e6b6f8d3903e7418
asjir <pa.paradysz@gmail.com>
2024-02-27 12:33:08 -0500

    delete dnd & pako

--- supabase-sveltekit/src/lib/trpc/client.ts
+++ supabase-sveltekit/src/lib/trpc/client.ts
@@ -1,8 +1,6 @@
 // lib/trpc/client.ts
 import type { Router } from '$lib/trpc/router';
-import { httpBatchLink } from '@trpc/client';
 import { createTRPCClient, type TRPCClientInit } from 'trpc-sveltekit';
-import { PakoTransformer } from './util';
 
 let browserClient: ReturnType<typeof createTRPCClient<Router>>;
 
@@ -10,7 +8,7 @@ export function trpc(init?: TRPCClientInit) {
 	const isBrowser = typeof window !== 'undefined';
 	if (isBrowser && browserClient) return browserClient;
 	const client = createTRPCClient<Router>({
-		init, transformer: PakoTransformer
+		init
 	});
 	if (isBrowser) browserClient = client;
 	return client;
