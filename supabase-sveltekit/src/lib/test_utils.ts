4cd4c8ee3184e048213e4ccea76c336496d6a988
asjir <pa.paradysz@gmail.com>
2024-02-28 19:19:26 +0000

    all format

--- supabase-sveltekit/src/lib/test_utils.ts
+++ supabase-sveltekit/src/lib/test_utils.ts
@@ -1,2 +1,2 @@
-import {JSDOM} from "jsdom"
-export const htmlstr2body = (h: string) => new JSDOM(h).window.document.body
+import { JSDOM } from "jsdom";
+export const htmlstr2body = (h: string) => new JSDOM(h).window.document.body;
--- supabase-sveltekit/src/routes/notes/queue/util.test.ts
+++ supabase-sveltekit/src/routes/notes/queue/util.test.ts
@@ -1,5 +1,12 @@
 import { expect, test } from "vitest";
-import { day, dequeue, enqueue, fromDay, nextIntervals, schedule } from "./util";
+import {
+  day,
+  dequeue,
+  enqueue,
+  fromDay,
+  nextIntervals,
+  schedule,
+} from "./util";
 import { setHours, differenceInCalendarDays } from "date-fns";
 import * as fd from "date-fns/fp";
 import type { Cards } from "$lib/dbtypes";
@@ -12,17 +19,20 @@ test("diff", () => {
   let z = day()(new Date());
   console.log(z, y);
   expect(differenceInCalendarDays(z, y)).toBe(1);
-  z = day()(fd.addDays(1)(fromDay()(z)))
+  z = day()(fd.addDays(1)(fromDay()(z)));
   console.log(z, y);
   expect(differenceInCalendarDays(z, y)).toBe(2);
 });
 
-test("a", () =>{
-  const yesterday = fromDay(7)(day()(fd.subDays(1)(new Date()))).toUTCString()
-  const card1: Cards = {last_review: yesterday, state: State.Learning} as Cards
-  expect(nextIntervals(card1)).toStrictEqual([1,3,9])
-  const card2 = {...card1, state: State.Review}
-  expect(nextIntervals(card2)).toStrictEqual([1,2,3])
-  expect(schedule(card1, 2).state).toBe(State.Review)
-  expect(nextIntervals(enqueue(dequeue(card1)))).toStrictEqual([1,3,9])
-})
+test("a", () => {
+  const yesterday = fromDay(7)(day()(fd.subDays(1)(new Date()))).toUTCString();
+  const card1: Cards = {
+    last_review: yesterday,
+    state: State.Learning,
+  } as Cards;
+  expect(nextIntervals(card1)).toStrictEqual([1, 3, 9]);
+  const card2 = { ...card1, state: State.Review };
+  expect(nextIntervals(card2)).toStrictEqual([1, 2, 3]);
+  expect(schedule(card1, 2).state).toBe(State.Review);
+  expect(nextIntervals(enqueue(dequeue(card1)))).toStrictEqual([1, 3, 9]);
+});
