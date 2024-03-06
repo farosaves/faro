bc22a545bdbdfff903787ca69e2df5bf68a4ec10
asjir <pa.paradysz@gmail.com>
2024-03-06 19:41:53 +0100

    aw yeah

--- supabase-sveltekit/src/routes/api/version/+server.ts
+++ supabase-sveltekit/src/routes/api/version/+server.ts
@@ -1,9 +1,8 @@
-import { execSync } from "child_process"
+import { COMMIT_UUID } from "$env/static/private";
 
 export const GET = () => {
     try {
-        const lastCommitUUID = execSync('git rev-parse HEAD').toString().trim();
-        return new Response(JSON.stringify({ version: lastCommitUUID }));
+        return new Response(JSON.stringify({ version: COMMIT_UUID }));
         // return new Response(lastCommitUUID.toString());
     } catch (error) {
         return new Response('Error retrieving version', { status: 500 });
