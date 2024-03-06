dfaa216e7b9ddd558c1619dc29005938f190b21d
asjir <pa.paradysz@gmail.com>
2024-03-06 19:23:43 +0100

    test version on dev is current

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 
+const dev = "https://dev.farosapp.com/"
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(dev);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Faros/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
+  const resp = await page.goto(dev+"api/version");
+  const versionData = await resp?.json(); 
+  expect(versionData?.version).toBe(process.env.COMMIT_UUID);
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
