dbdde9d0bc37481d7595192d769f4b9e85cd2aa7
asjir <pa.paradysz@gmail.com>
2024-03-08 11:06:48 +0100

    container working locally should be last deployment pre sextension

--- oxygen/flask/app.py
+++ oxygen/flask/app.py
@@ -1,17 +1,23 @@
+import json
 from flask import Flask, request, send_file, send_from_directory
-import subprocess, uuid, os, glob
-from time import sleep
+import subprocess, uuid, os, glob, boto3
 
 if not os.path.isdir("/tmp/appey"):
     os.mkdir("/tmp/appey")
 app = Flask(__name__)
 
 mydir = "/tmp/appey/"
-u = None
+u = uuid.uuid4()
+
+
+s3 = boto3.resource('s3',
+  endpoint_url = 'https://362e2d4a4b780aab43b3c82f2a779a47.r2.cloudflarestorage.com',
+  region_name = 'eeur'
+)
 
 @app.get('/last')
 def greet():
-    return send_from_directory(f"{mydir}{u}.html") if u else "hello"
+    return send_from_directory(mydir, f"{u}.html") if u else "jey"
 
 
 @app.post('/')
@@ -24,20 +30,17 @@ def run_pdf_conversion():
     global u
     u = uuid.uuid4()
 
+
     with open(f"{mydir}{u}.pdf", "wb+") as f:
         f.write(pdf_data)
 
     subprocess.run(["pdf2htmlEX", "--zoom", "1.3", "--dest-dir", mydir, f"{mydir}{u}.pdf", f"{u}.html"])
+    s3.Bucket("pdf2html").upload_file(f"{mydir}{u}.html", f"{u}.html")
 
-    with open(f"{mydir}{u}.html", "rb+") as f:
-        html = f.read()
-
-    print(len(html))
-
-    return "Conversion done!"
+    return json.dumps({"pdf_id": str(u)})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port="2227", debug=True)  # Listen on all interfaces for Docker
+    app.run(host='0.0.0.0', port=2227, debug=True)  # Listen on all interfaces for Docker
 
 
