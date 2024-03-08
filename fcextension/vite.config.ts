fc6adaf11c7a2820c8f283e5bf1c62de82222b8f
asjir <pa.paradysz@gmail.com>
2024-03-08 11:06:48 +0100

    container working locally should be last deployment pre sextension

--- fcextension/vite.config.ts
+++ fcextension/vite.config.ts
@@ -1,44 +1,23 @@
 import { crx } from "@crxjs/vite-plugin"
 import manifest from "./manifest.json"
 import { defineConfig } from "vite"
-import path from "path"
-import { svelte } from "@sveltejs/vite-plugin-svelte"
+import { sveltekit } from "@sveltejs/kit/vite"
+import path from "path";
 
-// const config = defineConfig(({ command, mode, ssrBuild }) => {
-// 	return {
-// 		plugins: (() => {
-// 			if (mode === "sveltekit"){
-// 				return [sveltekit()]
-// 			}
-// 			else if (mode === "crx"){
-// 				return [crx({ manifest })]
-// 			}
-// 			else{
-// 				return []
-// 			}})(),
-// 	};
-// });
-
-// export default config;
 export default defineConfig(({ command, mode }) => ({
-  plugins: [
-    svelte(
-      { emitCss: false }, // don't emit CSS to Vite
-    ),
-    crx({ manifest }),
-  ],
-  build: {
-    rollupOptions: {
-      external: [
-        "chrome",
-        "rangy-core.min.js",
-        "rangy-classaplier.min.js",
-        "rangy-highlighter.min.js",
-      ],
-    },
-    minify: false,
-  },
-  resolve: {
-    alias: { $lib: path.resolve(__dirname, "./src/lib") },
-  },
-}))
+	plugins: [mode == 'crx' ? crx({ manifest }) : sveltekit()],
+	build: {
+		rollupOptions: {
+			external: [
+				'chrome',
+				'rangy-core.min.js',
+				'rangy-classaplier.min.js',
+				'rangy-highlighter.min.js'
+			]
+		},
+		minify: false
+	},
+	resolve: {
+		alias: {'$lib': path.resolve(__dirname, './src/lib'),}
+	}
+}));
