e6608f9dc2f06e2e5389ed35723f26cd0fb300af
asjir <pa.paradysz@gmail.com>
2024-03-11 11:21:28 +0100

    works

--- supabase-sveltekit/src/routes/dashboard/+page.svelte
+++ supabase-sveltekit/src/routes/dashboard/+page.svelte
@@ -1,74 +1,100 @@
 <script lang="ts">
   import { onMount } from "svelte"
   import Note from "$lib/components/Note.svelte"
-  import { NoteSync } from "shared"
+  import { NoteSync, getTitlesUrls } from "shared"
   import Search from "$lib/components/Search.svelte"
   import type { NoteEx } from "shared"
   import DomainFilter from "$lib/components/DomainFilter.svelte"
-  import { identity, flow } from "fp-ts/lib/function"
+  import { identity, flow, pipe } from "fp-ts/lib/function"
   import { redirect } from "@sveltejs/kit"
   import LoginPrompt from "$lib/components/LoginPrompt.svelte"
   import { option as O } from "fp-ts"
   import { handlePayload, type NoteFilter } from "$lib/utils"
   import { sessStore } from "shared"
   import TagView from "$lib/components/TagView.svelte"
+  import { readable, writable } from "svelte/store"
   export let data
   $: ({ session: _session, supabase } = data)
   $: if (_session) $sessStore = O.some(_session)
   $: session = $sessStore
 
-  let showing_contents: boolean[][]
-  let note_sync: NoteSync = new NoteSync(supabase, undefined)
+  // let showing_contents: boolean[][]
+  let showing_contents: boolean[] = []
+  let note_sync: O.Option<NoteSync> = O.none
+  // let ns = flip(O.map)(note_sync)
+  let ns = <T,>(f: (a: NoteSync) => T) => O.map(f)(note_sync)
+  $: ns = <T,>(f: (a: NoteSync) => T) => O.map(f)(note_sync)
+
   let filterSortFun = (n: NoteEx) => {
     return { ...n, priority: Date.parse(n.created_at) }
   }
   let tagFilter: NoteFilter = identity
   let domainFilter: NoteFilter = identity
-  let note_groups = note_sync.get_groups(flow(filterSortFun, tagFilter, domainFilter))
-  $: note_groups = note_sync.get_groups(flow(filterSortFun, tagFilter, domainFilter))
+  let note_groups = pipe(
+    note_sync,
+    O.match(
+      () => readable([]),
+      (x) => x.get_groups(flow(filterSortFun, tagFilter, domainFilter)),
+    ),
+  )
+  // let note_groups = ns((x) => x.get_groups(flow(filterSortFun, tagFilter, domainFilter)))
+  $: note_groups = pipe(
+    note_sync,
+    O.match(
+      () => readable([]),
+      (x) => x.get_groups(flow(filterSortFun, tagFilter, domainFilter)),
+    ),
+  )
   onMount(async () => {
     session || redirect(302, "login")
-    note_sync.user_id = O.toNullable(session)?.user.id
-    note_sync.sb = supabase
-    note_sync.sb
-      .channel("notes")
-      .on(
-        "postgres_changes",
-        {
-          event: "*",
-          schema: "public",
-          table: "notes",
-          filter: `user_id=eq.${note_sync.user_id}`,
-        }, // at least url should be the same so no need to filter
-        handlePayload(note_sync),
-      )
-      .subscribe()
-    note_sync.update_all_pages()
+    const uid = O.toNullable(session)?.user.id
+    note_sync = O.some(new NoteSync(supabase, uid!))
+    ns((x) => (x.sb = supabase))
+    ns((note_sync) =>
+      note_sync.sb
+        .channel("notes")
+        .on(
+          "postgres_changes",
+          {
+            event: "*",
+            schema: "public",
+            table: "notes",
+            filter: `user_id=eq.${note_sync.user_id}`,
+          }, // at least url should be the same so no need to filter
+          handlePayload(note_sync),
+        )
+        .subscribe(),
+    )
+    await ns((x) => x.refresh_sources())
+    ns((x) => x.refresh_notes())
+    console.log(ns((x) => x.stuMap))
   })
 
   let close_all_notes = () => {
-    showing_contents = $note_groups.map(([t, note_group]) => note_group.map((_) => false))
+    showing_contents = showing_contents.map((v) => false)
   }
   close_all_notes()
   let safeget = <T,>(a: T[][], i: number) => (i in a ? a[i] : [])
 
   let w_rem = 16
-  let all_notes = note_sync.notestore
-  $: flat_notes = Object.entries($all_notes).flatMap(([s, v]) => v)
-  const handle_keydown = (e: KeyboardEvent) => {
-    if (e.metaKey && e.key === "z") {
-      e.preventDefault()
-      note_sync.restoredelete()
-      // (e.shiftKey ? redo : undo)();
-    }
-  }
+  let flat_notes = ns((x) => x.notestore)
+  $: flat_notes = ns((x) => x.notestore)
+  // const handle_keydown = (e: KeyboardEvent) => {
+  //   if (e.metaKey && e.key === "z") {
+  //     e.preventDefault()
+  //     note_sync.restoredelete()
+  //     // (e.shiftKey ? redo : undo)();
+  //   }
+  // }
 </script>
 
-<svelte:window on:keydown={handle_keydown} />
+<!-- <svelte:window on:keydown={handle_keydown} /> -->
 
 <LoginPrompt {session} />
-{Object.entries($all_notes).flatMap(([a, b]) => b).length}
-<TagView {note_sync} bind:tagFilter />
+<!-- {Object.entries($flat_notes).flatMap(([a, b]) => b).length} -->
+{#if O.isSome(note_sync)}
+  <TagView note_sync={note_sync.value} bind:tagFilter />
+{/if}
 <label for="my-drawer" class="btn btn-primary drawer-button md:hidden"> Open drawer</label>
 <div class="drawer md:drawer-open">
   <input id="my-drawer" type="checkbox" class="drawer-toggle" />
@@ -86,9 +112,9 @@
               <!-- bind:showing_content={showing_contents[i][j]} -->
               <Note
                 note_data={note}
-                showing_content={safeget(showing_contents, i)[j]}
+                showing_content={showing_contents[note.id]}
                 {close_all_notes}
-                {note_sync}
+                note_sync={note_sync.value}
                 {w_rem} />
             {/each}
           </div>
@@ -98,19 +124,20 @@
     <!-- class:hidden={get(note_sync.note_del_queue).length == 0}> -->
     <div class="toast toast-end z-10">
       <!-- {$note_del_queue.length} -->
-      <div class="alert alert-info">
+      <!-- <div class="alert alert-info">
         <button on:click={note_sync.restoredelete}>Undo last delete.</button>
-      </div>
+      </div> -->
     </div>
   </div>
   <div class="drawer-side z-10">
     <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
     <ul class="menu p-4 w-72 min-h-full bg-base-200 text-base-content">
-      <li>
+      <!-- <li>
         <Search bind:filterSortFun notes={flat_notes} />
-      </li>
-      <!-- <li><TagFilter all_tags={note_sync.alltags()} bind:tagFilter /></li> -->
-      <li><DomainFilter {note_sync} bind:domainFilter /></li>
+      </li> -->
+      {#if O.isSome(note_sync)}
+        <li><DomainFilter {note_groups} bind:domainFilter /></li>
+      {/if}
     </ul>
   </div>
 </div>
