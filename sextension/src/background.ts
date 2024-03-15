d59d14219591a9c3d1509af9da29887cff29cd44
asjir <pa.paradysz@gmail.com>
2024-03-15 16:43:09 +0100

    emitter here

--- sextension/src/background.ts
+++ sextension/src/background.ts
@@ -1,8 +1,56 @@
 import type { MockNote } from "$lib/utils"
 import { option as O } from "fp-ts"
+import { initTRPC } from "@trpc/server"
+import { observable, type Observer } from "@trpc/server/observable"
+import { createChromeHandler } from "trpc-chrome/adapter"
+import { z } from "zod"
+import { Emitter } from "$lib/emitter"
+import { getOrElse } from "shared"
+import { pipe } from "fp-ts/lib/function"
 
 const DOMAIN = import.meta.env.VITE_PI_IP.replace(/\/$/, "") // Replace with your domain
 const DEBUG = import.meta.env.DEBUG || false
+
+const t = initTRPC.create({
+  isServer: false,
+  allowOutsideOfServer: true,
+})
+
+const addZ = z.tuple([z.number(), z.number()])
+type AddZ = z.infer<typeof addZ>
+
+const pushsub = <T>() => {
+  const ee = new Emitter<T>()
+  const push = (x: T) => ee.emit("bob", x)
+  const sub = () => {
+    // return an `observable` with a callback which is triggered immediately
+    return observable<T>((emit) => {
+      const onAdd = emit.next
+      // trigger `onAdd()` when `add` is triggered in our event emitter
+      ee.on("bob", onAdd)
+      // unsubscribe function when client disconnects or stops subscribing
+      return () => {
+        ee.off("bob", onAdd)
+      }
+    })
+  }
+  return [push, sub]
+}
+const [pushAdd, subAdd] = pushsub()
+const appRouter = t.router({
+  add: t.procedure.input(addZ).query(({ input }) => {
+    pushAdd(input)
+    return input[0] + input[1]
+  }),
+  onAdd: t.procedure.subscription(subAdd),
+})
+
+export type AppRouter = typeof appRouter
+
+// @ts-expect-error namespace missing
+createChromeHandler({
+  router: appRouter,
+})
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
@@ -11,7 +59,7 @@ chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
   // closes the window
   if (/farosapp\.com\/account/.test(tab.url || ""))
     chrome.sidePanel.setOptions({ enabled: false }).then(() => chrome.sidePanel.setOptions({ enabled: true }))
-  chrome.runtime.sendMessage({ action: "update_cu∂rr_url" }).catch((e) => console.log(e))
+  chrome.runtime.sendMessage({ action: "update_curr_url" }).catch((e) => console.log(e))
 })
 
 chrome.tabs.onActivated.addListener((info) => {
--- shared/src/lib/fp-util.ts
+++ shared/src/lib/fp-util.ts
@@ -1,4 +1,4 @@
-import type { LazyArg } from "fp-ts/lib/function"
+import { pipe, type LazyArg } from "fp-ts/lib/function"
 import { option as O } from "fp-ts"
 
 export const getOrElse: <A>(onNone: LazyArg<NoInfer<A>>) => (ma: O.Option<A>) => A = O.getOrElse
