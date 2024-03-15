93d18b261b02ceb20ea1a9d558db2c7a8a202ede
asjir <pa.paradysz@gmail.com>
2024-03-15 13:51:25 +0100

    no adding

--- sextension/src/lib/note_mut.ts
+++ sextension/src/lib/note_mut.ts
@@ -1,65 +1,99 @@
-import { NoteSync, hostname, logIfError, type SourceData } from "shared"
+import { persisted } from "svelte-persisted-store"
+import {
+  NoteSync,
+  domain_title,
+  getOrElse,
+  hostname,
+  idx,
+  logIfError,
+  type Notes,
+  type SourceData,
+} from "shared"
 import { createMock, type MockNote } from "./utils"
-import { option as O, record as R, string as S, array as A } from "fp-ts"
-import { pipe, flow } from "fp-ts/lib/function"
-import { derived } from "svelte/store"
+import { option as O, record as R, string as S, array as A, tuple as T } from "fp-ts"
+import { pipe, flow, flip, identity } from "fp-ts/lib/function"
+import { derived, get, writable, type Readable, type Writable } from "svelte/store"
+import { invertLast } from "fp-ts-std/Record"
 type Src = SourceData["sources"]
 
-R.getEq(S.Eq)
+const hostnameStr = (url: string) => O.getOrElse(() => "")(hostname(url))
 
 export class NoteMut {
   ns: NoteSync
-  curr_source: O.Option<[Src, number]>
+  curr_source: Writable<O.Option<[Src, number]>>
+  panel: Readable<Notes[]>
+  source_idStore
   constructor(ns: NoteSync) {
     this.ns = ns
-    this.curr_source = O.none
-  }
-
-  panel(id: number) {
-    return derived(
-      this.ns.notestore,
+    this.curr_source = writable(O.none)
+    this.panel = derived([this.ns.notestore, this.curr_source], ([ns, ots]) =>
+      pipe(
+        ots,
+        O.map((ts) => Object.values(ns).filter((n) => n.source_id == ts[1])),
+        getOrElse(() => []),
+      ),
+    )
+    this.source_idStore = derived(
+      ns.stuMapStore,
       flow(
-        Object.values,
-        A.filter((n) => n.source_id == id),
+        R.map(({ url, title }) => domain_title(url, title)),
+        R.compact,
+        invertLast(identity),
+        R.map(parseInt),
       ),
     )
   }
 
-  upSetSrcId = async (source: Src) => {
-    // extract source id (number) from curr_source
-    // if curr_source is some check Src matches and return the number, otherwise none
-    const optId = pipe(
-      this.curr_source,
+  _updateSrc = (source: Src, id: number) => {
+    this.curr_source.set(O.some([source, id]))
+    const { title, url } = source
+    this.ns.update_source(id, { sources: source })
+    return id
+  }
+
+  // get source if already present locally
+  localSrcId = (source: Src) => {
+    // get current
+    const optIdC = pipe(
+      get(this.curr_source),
       O.chain(O.fromPredicate(([src, id]) => R.getEq(S.Eq).equals(src, source))),
       O.map((t) => t[1]),
     )
-    if (O.isSome(optId)) return optId.value
-
-    // if (O.getEq(R.getEq(S.Eq)).equals(O.some(sources), this.curr_source)) return
+    if (O.isSome(optIdC)) return optIdC
+    // get store
+    const { url, title } = source
+    const optId = O.chain((dt: string) => idx(get(this.source_idStore), dt))(domain_title(url, title))
+    if (O.isSome(optId)) return O.some(this._updateSrc(source, optId.value))
+    // stil we're on not inserted so set to none:
+    this.curr_source.set(O.none)
+    return O.none
+  }
+  // get locally or db
+  _updatedSrcId = async (source: Src) => {
+    const localId = this.localSrcId(source)
+    if (O.isSome(localId)) return localId
     const { data } = await this.ns.sb
       .from("sources")
       .select("id")
-      .eq("domain", hostname(source.url))
+      .eq("domain", hostnameStr(source.url))
       .eq("title", source.title)
       .maybeSingle()
-    if (data) {
-      this.curr_source = O.some([source, data.id])
-      return data.id
-    }
+    if (data) return O.some(this._updateSrc(source, data.id))
+    return O.none
+  }
+  // get locally or db or insert: to be called when adding note
+  upSetSrcId = async (source: Src) => {
+    const oid = await this._updatedSrcId(source)
+    if (O.isSome(oid)) return oid.value
+    const { url, title } = source
+
     const { data: data2, error } = await this.ns.sb
       .from("sources")
-      .insert({
-        domain: hostname(source.url),
-        url: source.url,
-        title: source.title,
-      })
+      .insert({ domain: hostnameStr(url), url, title })
       .select("id")
       .maybeSingle()
       .then(logIfError)
-    if (data2) {
-      this.curr_source = O.some([source, data2.id])
-      return data2.id
-    }
+    if (data2) return this._updateSrc(source, data2.id)
     throw error // TODO: if doing offline needs to be options
   }
 
