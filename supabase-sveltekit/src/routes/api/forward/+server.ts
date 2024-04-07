dcfd6e97ab78481ce69dce14457372aed7c8de02
Pawel Paradysz <pa.paradysz@gmail.com>
2024-04-07 14:49:59 +0200

    tryna stringifying doesnt work

---
+++ supabase-sveltekit/src/routes/api/forward/+server.ts
@@ -0,0 +1,25 @@
+import { hostname } from "shared"
+
+export const GET = async ({ url }) => {
+//   const pageUrl = JSON.parse(url.searchParams.get("url") || "{}")
+  const pageUrl = JSON.parse(url.searchParams.get("url") || "{}") as string
+  console.log("page url", pageUrl)
+  const mainLoad = url.searchParams.get("main") || false
+  if (!mainLoad) {
+    const fetched = await fetch(pageUrl)
+    return new Response(await fetched.blob(), { headers: { "Content-Type": fetched.headers.get("Content-Type")! } })
+  }
+  // also adjust links
+  //   const host = hostname(pageUrl).value
+  const host = pageUrl.match(/https?:\/\/[^/]+\//u)![0].toString()
+  console.log("host", host)
+  const text = await fetch(pageUrl).then(x => x.text())
+  //   console.log(text)
+  //   const text2 = text.replaceAll(/="\/(?=\w)/g, "=\"/api/forward/?url=" + host)
+  //   const text3 = text2.replaceAll(/="\/api\/forward\/\?url=/g, "a")
+  const text3 = text.replaceAll(/(?<=="\/)/g, s => "api/forward/?url=" + JSON.stringify(host + s))
+  //   console.log(text2)
+  return new Response(text3)
+}
+
+
