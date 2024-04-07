a766f2bca08bf73168f9224d683ef2869b8b66e2
Pawel Paradysz <pa.paradysz@gmail.com>
2024-04-07 14:49:59 +0200

    tryna stringifying doesnt work

--- supabase-sveltekit/src/routes/notes/[note_id]/+page.svelte
+++ supabase-sveltekit/src/routes/notes/[note_id]/+page.svelte
@@ -2,10 +2,11 @@
   //   import { onMount } from "svelte"
   export let data
   import { page } from "$app/stores"
-  import { NoteSync } from "shared"
+  import { NoteSync, funLog, sleep } from "shared"
   import { get } from "svelte/store"
   import { record as R, map as M } from "fp-ts"
   import type { UUID } from "crypto"
+  import { onMount } from "svelte"
 
   let noteId: string
 
@@ -16,10 +17,39 @@
   const a = get(stuMapStore).get((n?.source_id as UUID) || "")
 
   //   R.lookup(noteId,
+  let div: HTMLDivElement
+  onMount(async () => {
+    const url = JSON.stringify("https://en.wikipedia.org/wiki/Kalanchoe")
+    div.innerHTML = await fetch(`/api/forward/?url=${url}&main=true`).then((x) => x.text())
+    const stylesheetLinks = Array.from(div.querySelectorAll('link[rel="stylesheet"]')) as HTMLLinkElement[]
+
+    //   fetch('http://www.example.com/external-styles.css')
+    // .then(response => response.text())
+    // .then(css => {
+    //   const styleElement = document.createElement('style');
+    //   styleElement.textContent = css;
+    //   document.head.appendChild(styleElement);
+
+    sleep(100)
+    console.log("ps", document.getElementsByTagName("p").length)
+    for (const link of stylesheetLinks) {
+      const resp = await fetch(JSON.stringify(link.href)).then()
+      resp.text().then(funLog("reps text"))
+    }
+
+    // fetch("https://en.wikipedia.org/wiki/Kalanchoe")
+    //   .then((response) => response.text())
+    //   .then((html) => {
+    //     div.innerHTML = html
+    //   })
+    //   .catch((error) => console.error("Error:", error))
+  })
 </script>
 
 <svelte:window on:mouseup={() => console.log(window.getSelection())} />
-hmm?
-<iframe width="100%" height="1000px" src={a?.url} />
+
+<div bind:this={div}></div>
+
+<!-- <iframe width="100%" height="1000px" src={a?.url} /> -->
 
 <!-- {JSON.stringify([...get(noteStore).keys()])} -->
