e93e1dd762c8499bc865782558bfc64db5eec1cd
Pawel Paradysz <pa.paradysz@gmail.com>
2024-04-08 10:39:58 +0200

    kept mhtml2html

--- supabase-sveltekit/src/routes/api/forward/+server.ts
+++ supabase-sveltekit/src/routes/api/forward/+server.ts
@@ -1,8 +1,8 @@
-import { hostname } from "shared"
+import { JSDOM } from "jsdom"
 
 export const GET = async ({ url }) => {
-  const pageUrl = (url.searchParams.get("url") || "") as string
-  //   const pageUrl = JSON.parse(url.searchParams.get("url") || "{}") as string
+  const pageUrl = decodeURIComponent(url.searchParams.get("url") || "")
+  // console.log("page url", pageUrl)
   const mainLoad = url.searchParams.get("main") || false
   if (!mainLoad) {
     const fetched = await fetch(pageUrl)
@@ -11,16 +11,15 @@ export const GET = async ({ url }) => {
   // also adjust links
   //   const host = hostname(pageUrl).value
   const host = pageUrl.match(/https?:\/\/[^/]+\//u)![0].toString()
-  console.log("host", host)
+  // console.log("host", host)
   const text = await fetch(pageUrl).then(x => x.text())
   //   console.log(text)
   //   const text2 = text.replaceAll(/="\/(?=\w)/g, "=\"/api/forward/?url=" + host)
   //   const text3 = text2.replaceAll(/="\/api\/forward\/\?url=/g, "a")
 
   const text3 = text.replaceAll(/(?<=="\/)[^"]+/g, (s) => {
-    console.log("capture:", s)
-    // return "api/forward/?url=" + JSON.stringify(host + s)
-    return "api/forward/?url=" + (host + s)
+    // console.log("jsdom", new JSDOM(s).window.document.body)
+    return "api/forward/?url=" + encodeURIComponent(host + s)
   })
   return new Response(text3)
 }
