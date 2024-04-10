2b55bf2edf4c5685cdf73ca091ba666b23e18c0a
Pawel Paradysz <pa.paradysz@gmail.com>
2024-04-10 12:16:30 +0200

    normalize modal, abandon stepping

---
+++ shared/src/lib/dashboard/components/CmModal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { modalOpenStore, modalStore, modalSub } from "$lib/stores"
+</script>
+
+<dialog id="modal$" class="modal" use:modalSub on:close={() => ($modalOpenStore = false)}>
+  <div class="modal-box">
+    <p class="py-4">
+      {$modalStore}
+    </p>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
