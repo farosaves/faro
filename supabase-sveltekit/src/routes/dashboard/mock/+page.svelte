d0f641e2ec73ee0ad80b2da4a82bf0fb01edbbd8
Pawel Paradysz <pa.paradysz@gmail.com>
2024-04-15 09:04:59 +0200

    from cli

--- supabase-sveltekit/src/routes/dashboard/mock/+page.svelte
+++ supabase-sveltekit/src/routes/dashboard/mock/+page.svelte
@@ -6,19 +6,19 @@
   import { onMount } from "svelte"
   import { get } from "svelte/store"
   import * as devalue from "devalue"
-  const noteSync = new NoteSync(supabase, undefined)
+  const noteSync = new NoteSync(supabase, undefined, async () => true)
   noteSync.noteStore = persisted("mockNoteStore", new Map(), { serializer: devalue })
   noteSync.stuMapStore = persisted("mockStuMapStore", new Map(), { serializer: devalue })
   noteSync.actionQueue.queueStore = persisted("mockActionQueue", new Map(), { serializer: devalue })
-  noteSync.actionQueue.queueStore.subscribe(as => as.length && noteSync.actionQueue.queueStore.set([]))
+  noteSync.actionQueue.queueStore.subscribe((as) => as.length && noteSync.actionQueue.queueStore.set([]))
   console.log(get(noteSync.noteStore))
   let showLoginPrompt = false
   onMount(async () => {
     console.log(mock)
     showLoginPrompt = M.size(new Map([...get(noteSync.noteStore), ...mock.notes])) > M.size(mock.notes)
     if (!showLoginPrompt) {
-      noteSync.noteStore.update(n => new Map([...n, ...mock.notes])) // use user changes to mock notes or just use them
-      noteSync.stuMapStore.update(n => mock.stuMap)
+      noteSync.noteStore.update((n) => new Map([...n, ...mock.notes])) // use user changes to mock notes or just use them
+      noteSync.stuMapStore.update((n) => mock.stuMap)
     }
   })
 </script>
