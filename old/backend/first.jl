c3be9b2930f7dfbc57833c3bec956eaa50b85f86
Pawel Paradysz <pa.paradysz@gmail.com>
2024-04-17 11:00:31 +0200

    moved old out of repo

--- old/backend/first.jl
+++
@@ -1,5 +0,0 @@
-using LFWBackend
-import LFWBackend: db
-LFWBackend.run()
-LFWBackend.db[LFWBackend.CardContent[]]
-using HTTP, JSON3
--- old/oldfcextension/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-
-
-export default defineConfig({
-  preprocess: vitePreprocess(),
-  plugins: [sveltekit()],
-})
