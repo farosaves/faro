a4dd8597f1dcab3bcd122f41276f5a24f07671ed
Pawel Paradysz <pa.paradysz@gmail.com>
2024-05-03 09:48:58 +0200

    removed upload mhtml

--- supabase-sveltekit/src/lib/trpc/api/mhtmly.ts
+++ supabase-sveltekit/src/lib/trpc/api/mhtmly.ts
@@ -1,16 +1,16 @@
-import { compressSync, strToU8 } from "fflate"
-import aws from "aws-sdk"
+// import { compressSync, strToU8 } from "fflate"
+// import aws from "aws-sdk"
 
-const { S3 } = aws
-// import { S3 } from "aws-sdk"
-import type { UUID } from "crypto"
-import { funLog } from "shared"
-const s3 = new S3({
-  endpoint: "https://362e2d4a4b780aab43b3c82f2a779a47.r2.cloudflarestorage.com",
-  region: "eeur",
-})
+// const { S3 } = aws
+// // import { S3 } from "aws-sdk"
+// import type { UUID } from "crypto"
+// import { funLog } from "shared"
+// const s3 = new S3({
+//   endpoint: "https://362e2d4a4b780aab43b3c82f2a779a47.r2.cloudflarestorage.com",
+//   region: "eeur",
+// })
 
-export const uploadMHTML = async (mhtml: string, id: UUID) => {
-  const bytes = compressSync(strToU8(mhtml))
-  s3.putObject({ Bucket: "mhtml", Key: id + ".mhtml.zip", Body: bytes }, funLog("uploadMHTML"))
-}
+// export const uploadMHTML = async (mhtml: string, id: UUID) => {
+//   const bytes = compressSync(strToU8(mhtml))
+//   s3.putObject({ Bucket: "mhtml", Key: id + ".mhtml.zip", Body: bytes }, funLog("uploadMHTML"))
+// }
