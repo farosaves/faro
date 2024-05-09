52c7f42322441ed8edb6a04ac37479e8e94e474a
Pawel Paradysz <pa.paradysz@gmail.com>
2024-05-09 16:09:05 +0100

    Revert "Dropdown tag view"

--- notes/Wording.md
+++
@@ -1,3 +0,0 @@
-Save & organize snippets - use like bookmarks
-Instead of bookmarking a whole page, you save an exact sentence. And when you open your save you go back to that sentence.
-Makes it easier to remember what's "in" your save and why you saved it.
--- shared/src/lib/dashboard/components/TagView.svelte
+++ shared/src/lib/dashboard/components/TagView.svelte
@@ -4,35 +4,41 @@
   import IconCheckbox from "~icons/tabler/checkbox"
   import IconTagOff from "~icons/tabler/tag-off"
 
-  import { flow } from "fp-ts/lib/function"
-  import { array as A, set as S, option as O, either as E, string as Str } from "fp-ts"
-  import { NoteDeri, tagModalOpenStore } from "shared"
+  import { flow, identity, pipe } from "fp-ts/lib/function"
+  import { array as A, record as R, nonEmptyArray as NA, option as O, tuple as T } from "fp-ts"
+  import { desc, NoteDeri, tagModalOpenStore } from "shared"
   import { derived } from "svelte/store"
-  import { exclTagSet, exclTagSets } from "../filterSortStores"
-  import { getGroupTagCounts } from "./tagViewStores"
-
+  import { exclTagSet, exclTagSets, twoPlusTags } from "../filterSortStores"
   export let noteDeri: NoteDeri
   // let noteStore = note_sync.noteStore
-  const tagsCounts = getGroupTagCounts(noteDeri)
-  const pureTags = derived(tagsCounts, flow(A.map(E.swap), A.map(O.fromEither), A.compact))
-  const tagGroups = derived(tagsCounts, flow(A.map(O.fromEither), A.compact))
+  const tags_counts = derived(noteDeri.noteArr, (x) =>
+    pipe(
+      x,
+      A.flatMap((note) => note.tags || []),
+      NA.groupBy(identity),
+      R.map((x) => x.length),
+      R.toArray,
+    )
+      .concat(
+        // prettier-ignore
+        [["", pipe(x, A.filter(note => !note.tags.length), A.size)]],
+      )
+      .toSorted(desc(([x, y]) => y)),
+  )
   const untagged = derived(
-    tagsCounts,
+    tags_counts,
     flow(
-      A.map(E.toUnion),
       A.findFirst((x) => x[0] == ""),
-      O.map((x) => x[1]),
+      O.map(T.snd),
       O.getOrElse(() => 0),
     ),
   )
-  const allTags = derived(noteDeri.allTags, A.append(""))
-  // const allExcl = derived(exclTagSet, (s) => (s.size ? A.every(s.has) : (_a: any) => false))
-  const allExcl = derived(exclTagSet, (s) => A.reduce(true, (prev, a: string) => prev && s.has(a)))
 
+  // $: console.log(!!$tagFilter, "tagFilter updated")
   const checkClick = () => {
     // assigns to trigger potential $:
     if ($exclTagSet.size > 0) $exclTagSets.sets[$exclTagSets.currId] = new Set()
-    else $exclTagSets.sets[$exclTagSets.currId] = new Set($allTags)
+    else $exclTagSets.sets[$exclTagSets.currId] = new Set($tags_counts.map(([x, y]) => x))
   }
   // $: console.log(Array.from($exclTagSets.sets[$exclTagSets.currId]))
   const toggleTag = (tag: string) =>
@@ -40,22 +46,11 @@
       s.sets[s.currId].delete(tag) || s.sets[s.currId].add(tag)
       return s
     })
-  const toggleTagGroup = (tags: string[]) =>
-    exclTagSets.update((s) => {
-      if (S.intersection(Str.Eq)(s.sets[s.currId])(new Set(tags)).size)
-        for (const tag of tags) s.sets[s.currId].delete(tag)
-      else for (const tag of tags) s.sets[s.currId].add(tag)
-      // s.sets[s.currId].delete(tag) || s.sets[s.currId].add(tag)
-      return s
-    })
-  const onDblClick = (tag: string) => () => {
-    $exclTagSets.sets[$exclTagSets.currId] = new Set($allTags)
-    toggleTag(tag)
-  }
-  const onDblClickGroup = (tags: string[]) => () => {
-    $exclTagSets.sets[$exclTagSets.currId] = new Set($allTags)
-    toggleTagGroup(tags)
-  }
+  const onDblClick = (tag: string) => () =>
+    ($exclTagSets.sets[$exclTagSets.currId] = new Set(
+      $tags_counts.map(([x, y]) => x).filter((t) => t != tag),
+    ))
+
   // let modalPotential: boolean
   let myModal: HTMLDialogElement | null = null
   let currTag: string
@@ -77,29 +72,17 @@
     myModal && myModal.close()
     noteDeri.sync.tagUpdate(currTag, O.none)
   }
-  let dropdownOpen = false
-  let hRem = 2
-  // window.yo = () => (dropdownOpen = !dropdownOpen)
 </script>
 
-<div class="bg-base-200 sticky top-0 z-20 carousel w-[99%]" style="height: {hRem}rem">
+<div class="bg-base-200 sticky top-0 z-20 carousel w-[99%]">
   <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip="toggle all">
-    <details class="dropdown" bind:open={dropdownOpen}>
-      <summary
-        class="btn btn-neutral btn-sm text-nowrap"
-        on:click|preventDefault={checkClick}
-        class:btn-outline={$exclTagSet.size}>
-        <!-- <button -->
-        <IconCheckbox />
-        <!-- </button> -->
-      </summary>
-      <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-        <li>yo</li>
-        <li>ya</li>
-      </ul>
-    </details>
+    <button
+      class="btn btn-neutral btn-sm text-nowrap"
+      on:click={checkClick}
+      class:btn-outline={$exclTagSet.size}>
+      <IconCheckbox />
+    </button>
   </div>
-
   <!-- <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip="2+ tags">
     <button
       class="btn btn-neutral btn-sm text-nowrap"
@@ -119,20 +102,7 @@
       </button>
     </div>
   {/if}
-  {#each $tagGroups.filter((x) => x[0].length > 0) as [tag, cnt, tss]}
-    {@const ts = tss.map((t) => t[0])}
-    <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip={cnt}>
-      <button
-        class="btn btn-neutral btn-sm text-nowrap"
-        on:click={() => toggleTagGroup(ts)}
-        on:contextmenu|preventDefault={onContextMenu(tag)}
-        on:dblclick={onDblClickGroup(ts)}
-        class:btn-outline={$allExcl(ts)}
-        >{tag}/
-      </button>
-    </div>
-  {/each}
-  {#each $pureTags.filter((x) => x[0].length > 0) as [tag, cnt]}
+  {#each $tags_counts.filter((x) => x[0].length > 0) as [tag, cnt]}
     <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip={cnt}>
       <button
         class="btn btn-neutral btn-sm text-nowrap"
