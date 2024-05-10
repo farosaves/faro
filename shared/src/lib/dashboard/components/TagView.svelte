67d85573cf00f101f2fc55d7a7ad3d0feca8ed6b
Pawel Paradysz <pa.paradysz@gmail.com>
2024-05-10 09:55:28 +0200

    yee

--- shared/src/lib/dashboard/components/TagView.svelte
+++ shared/src/lib/dashboard/components/TagView.svelte
@@ -88,22 +88,22 @@
 </script>
 
 <div class="bg-base-200 sticky top-0 z-20 carousel w-[99%]" style="height: {hRem}rem">
-  <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip="toggle all">
-    <details class="dropdown" bind:open={dropdownOpen}>
-      <summary
-        class="btn btn-neutral btn-sm text-nowrap"
-        on:click|preventDefault={checkClick}
-        class:btn-outline={$exclTagSet.size}>
-        <!-- <button -->
-        <IconCheckbox />
-        <!-- </button> -->
-      </summary>
-      <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+  <!-- <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip="toggle all">
+    <details class="dropdown" bind:open={dropdownOpen}> -->
+  <button
+    class="btn btn-neutral btn-sm text-nowrap"
+    on:click|preventDefault={checkClick}
+    class:btn-outline={$exclTagSet.size}>
+    <!-- <button -->
+    <IconCheckbox />
+    <!-- </button> -->
+  </button>
+  <!-- <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
         <li>yo</li>
         <li>ya</li>
       </ul>
     </details>
-  </div>
+  </div> -->
 
   <!-- <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip="2+ tags">
     <button
@@ -138,32 +138,6 @@
       </button>
     </div>
   {/each}
-
-  <!-- {#each $tagGroups.filter((x) => x[0].length > 0) as [tag, cnt, tss]}
-    {@const ts = tss.map((t) => t[0])}
-    <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip={cnt}>
-      <button
-        class="btn btn-neutral btn-sm text-nowrap"
-        on:click={() => _toggleTagGroup(ts)}
-        on:contextmenu|preventDefault={onContextMenu(tag)}
-        on:dblclick={onDblClickGroup(ts)}
-        class:btn-outline={$allExcl(ts)}
-        >{tag}/
-      </button>
-    </div>
-  {/each}
-  {#each $pureTags.filter((x) => x[0].length > 0) as [tag, cnt]}
-    <div class="tooltip tooltip-right tooltip-secondary carousel-item" data-tip={cnt}>
-      <button
-        class="btn btn-neutral btn-sm text-nowrap"
-        on:click={() => _toggleTag(tag)}
-        on:contextmenu|preventDefault={onContextMenu(tag)}
-        on:dblclick={onDblClick(tag)}
-        class:btn-outline={$exclTagSet.has(tag)}
-        >{tag}
-      </button>
-    </div>
-  {/each} -->
 </div>
 
 <dialog class="modal" bind:this={myModal} on:close={() => ($tagModalOpenStore = false)}>
