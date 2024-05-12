cf6d95272bfe6e852b284a200de0a12bf21f6472
Pawel Paradysz <pa.paradysz@gmail.com>
2024-05-12 16:33:37 +0200

    2 more lines

--- supabase-sveltekit/src/routes/welcome/+page.svelte
+++ supabase-sveltekit/src/routes/welcome/+page.svelte
@@ -11,8 +11,17 @@
         <kbd class="kbd">D</kbd>
       </p>
       <p class="py-2">Highlight this <i>word</i> and save again.</p>
-      <p class="py-2">They'll upload to the cloud once you</p>
-      <button class="py-2 btn btn-primary">Log in</button>
+      <p class="py-2">
+        Find them on the dashboard, you can open it with <kbd class="kbd">{altKey}</kbd> +
+        <kbd class="kbd">F</kbd>
+      </p>
+      <p class="py-2">
+        They'll upload to the cloud once you: <button class="py-2 btn btn-primary">Log in</button>
+      </p>
+      <p class="py-2">
+        You can make an account whenever you're ready.<br />
+        Until then, the dashboard on the website here won't work - only the in-extension one.
+      </p>
     </div>
   </div>
 </div>
