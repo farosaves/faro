bb24d5483b3dc3ff804d4ab60bebe163595040d6
Pawel Paradysz <pa.paradysz@gmail.com>
2024-05-13 12:46:19 +0200

    wording, bg hopefully fixed, welcome, click title

--- supabase-sveltekit/src/routes/welcome/+page.svelte
+++ supabase-sveltekit/src/routes/welcome/+page.svelte
@@ -2,17 +2,24 @@
   import { altKey } from "shared"
 </script>
 
-<div class="hero min-h-screen bg-base-100">
+<svelte:head>
+  <title>Faros - Welcome</title>
+</svelte:head>
+
+<div class="hero min-h-screen bg-base-300">
   <div class="hero-content text-center">
     <div class="max-w-lg">
       <h1 class="py-2 text-5xl font-bold">Welcome onboard!</h1>
+      <h2 class="py-2 text-3xl font-bold">Try it out</h2>
       <p class="py-2">
         Highlight this sentence (triple clicking) and save with <kbd class="kbd">{altKey}</kbd> +
         <kbd class="kbd">D</kbd>
       </p>
       <p class="py-2">Highlight this <i>word</i> and save again.</p>
+      <p class="py-2">Double click your saves in the panel on the right to get back to them.</p>
+
       <p class="py-2">
-        Find them on the dashboard, you can open it with <kbd class="kbd">{altKey}</kbd> +
+        Also find them on the dashboard, open it with <kbd class="kbd">{altKey}</kbd> +
         <kbd class="kbd">F</kbd>
       </p>
       <p class="py-2">
