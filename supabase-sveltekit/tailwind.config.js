06654204fa306e729b954705c5d68d177620c5b9
Pawel Paradysz <pa.paradysz@gmail.com>
2024-05-22 20:17:15 +0200

    secondary picked

--- supabase-sveltekit/tailwind.config.js
+++ supabase-sveltekit/tailwind.config.js
@@ -21,7 +21,7 @@ export default {
           "primary": "oklch(61.77% 0.163 307.59)",
           // secondary: "oklch(53.53% 0.2365 269.97)",
           // secondary: "oklch(70.58% 0.1581 252)", // ph
-          "secondary": "oklch(62.94% 0.22 252)", // id rather do reddish..
+          "secondary": "oklch(75.72% 0.1387 252)", // id rather do reddish..
           "base-100": "oklch(20.77% 0.06 265.75)", // #1A103D  // #1A103D
         },
       },
