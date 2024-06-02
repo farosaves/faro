7c30e6fdfa53bde648b04bd5aa333649361f5926
Pawel Paradysz <pa.paradysz@gmail.com>
2024-06-02 10:43:20 +0200

    forms, forms, forms.

--- supabase-sveltekit/src/routes/account/sign-up/+page.server.ts
+++ supabase-sveltekit/src/routes/account/sign-up/+page.server.ts
@@ -5,11 +5,8 @@ import { funLog } from "shared"
 
 export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
   const { session } = await safeGetSession().then(funLog("login page srvr load get sess"))
-
-  // if the user is already logged in return them to the dashboard? page
   if (session) {
     throw redirect(303, "/dashboard")
   }
-
   return { url: url.origin }
 }
