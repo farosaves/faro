a55da915522a3f4ee701843a1b33d3fa77c4662a
Pawel Paradysz <pa.paradysz@gmail.com>
2024-06-02 12:45:14 +0200

    moved stuff around - redirect still bad

--- supabase-sveltekit/src/routes/account/login/+layout.server.ts
+++ supabase-sveltekit/src/routes/account/login/+layout.server.ts
@@ -1,9 +1,9 @@
 // src/routes/+page.server.ts
 import { redirect } from "@sveltejs/kit"
-import type { PageServerLoad } from "./$types"
+import type { LayoutServerLoad } from "./$types"
 import { funLog } from "shared"
 
-export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
+export const load: LayoutServerLoad = async ({ url, locals: { safeGetSession } }) => {
   const { session } = await safeGetSession().then(funLog("login page srvr load get sess"))
   if (session) {
     throw redirect(303, "/dashboard")
