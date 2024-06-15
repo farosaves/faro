40f835e0c06426914705a7fa97878a8f739a1466
Pawel Paradysz <pa.paradysz@gmail.com>
2024-06-15 16:41:01 +0200

    PUBMED

--- shared/src/lib/dashboard/utils.ts
+++ shared/src/lib/dashboard/utils.ts
@@ -3,6 +3,6 @@ import type { NoteEx } from "$lib/db/typeExtras"
 export const gotoFunction = (nd: NoteEx | undefined) => () => {
   if (nd === undefined) return
   const url = new URL(nd.url)
-  nd.snippet_uuid && url.searchParams.set("highlightUuid", nd.snippet_uuid)
+  nd.snippet_uuid && (url.hash = "_" + nd.snippet_uuid)
   url && open(url)
 }
