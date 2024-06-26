a4a526f0c3bb8951d39f3cfa34f29670a76cb097
Pawel Paradysz <pa.paradysz@gmail.com>
2024-06-26 11:06:37 +0200

    Service worker attempt

---
+++ supabase-sveltekit/src/routes/setup/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount } from "svelte"
+  let err = ""
+  onMount(async () => {})
+</script>
+
+<svelte:head>
+  <title>Faros - Setup</title>
+</svelte:head>
+
+<div class="flex w-full justify-center">
+  <button
+    class="btn btn-primary"
+    on:click={async () => {
+      if (typeof self.showDirectoryPicker == typeof undefined) {
+        err = "file access not supported"
+        return
+      }
+      window.dirHandle = await self.showDirectoryPicker({
+        startIn: "pictures",
+      })
+    }}>Start</button>
+</div>
+
+{err}
