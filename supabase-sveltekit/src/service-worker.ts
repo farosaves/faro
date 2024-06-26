b880417f6a290548758cc6b6d6e57c5be1e05f9c
Pawel Paradysz <pa.paradysz@gmail.com>
2024-06-26 11:14:15 +0200

    dont cache ext

--- supabase-sveltekit/src/service-worker.ts
+++ supabase-sveltekit/src/service-worker.ts
@@ -40,6 +40,7 @@ sw.addEventListener("activate", (event) => {
 sw.addEventListener("fetch", (event) => {
   // ignore POST requests etc
   if (event.request.method !== "GET") return
+  if (!event.request.url.startsWith("http")) return
 
   async function respond() {
     const url = new URL(event.request.url)
