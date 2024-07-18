e7997ce233fbf6058d50546b7d459b245d8aa0c2
Pawel Paradysz <pa.paradysz@gmail.com>
2024-07-18 10:59:03 +0200

    cleaner as doesnt modify the dom, but then hover and disappearing transitions have same duration

--- supabase-sveltekit/src/routes/+page.svelte
+++ supabase-sveltekit/src/routes/+page.svelte
@@ -78,20 +78,21 @@
     </div>
   </div>
   <div
-    class=" left-1/2 bottom-2 absolute hidden xl:inline transform -translate-x-1/2 -translate-y-1/2 text-center opacity-40 hover:opacity-80 transition-opacity">
-    {#if $scrolled < 40}<a href="#key-features" transition:fade>
-        <svg
-          class="size-16 opacity-35 m-auto cursor-pointer"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="-5 -8 24 24"
-          width="28"
-          fill="currentColor"
-          ><path
-            d="M7.071 5.314l4.95-4.95a1 1 0 1 1 1.414 1.414L7.778 7.435a1 1 0 0 1-1.414 0L.707 1.778A1 1 0 1 1 2.121.364l4.95 4.95z"
-          ></path
-          ></svg>
-      </a>
-    {/if}
+    class=" left-1/2 bottom-2 absolute hidden xl:inline transform -translate-x-1/2 -translate-y-1/2 text-center opacity-40 hover:opacity-80 transition-opacity"
+    class:opacity-0={$scrolled > 40}
+    class:hover:opacity-0={$scrolled > 40}>
+    <a href="#key-features" transition:fade>
+      <svg
+        class="size-16 opacity-35 m-auto cursor-pointer"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="-5 -8 24 24"
+        width="28"
+        fill="currentColor"
+        ><path
+          d="M7.071 5.314l4.95-4.95a1 1 0 1 1 1.414 1.414L7.778 7.435a1 1 0 0 1-1.414 0L.707 1.778A1 1 0 1 1 2.121.364l4.95 4.95z"
+        ></path
+        ></svg>
+    </a>
   </div>
 </div>
 <div class="w-full pt-6 h-fit flex flex-col justify-center items-center" id="key-features">
