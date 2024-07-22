5ba3c55b753b2886e165247b06e3242547fcb98b
Pawel Paradysz <pa.paradysz@gmail.com>
2024-07-22 11:41:50 +0200

    updates notepanel

--- sextension/src/lib/components/NotePanel.svelte
+++ sextension/src/lib/components/NotePanel.svelte
@@ -2,7 +2,7 @@
   import type { NoteDeri, Notes, SyncLike } from "shared"
   import Note from "$lib/components/Note.svelte"
   import { option as O, array as A, string as S, eq } from "fp-ts"
-  import { asc, createMock, extractCharIdx, funLog, type PendingNote } from "shared"
+  import { altKey, asc, createMock, extractCharIdx, funLog, type PendingNote } from "shared"
   import { flow, pipe } from "fp-ts/lib/function"
   import type { NoteMut } from "$lib/note_mut"
   import { derived, type Readable } from "svelte/store"
@@ -15,7 +15,6 @@
   )
 
   // prettier-ignore
-  // $: mocked = pipe(optimistic, O.map((r) => { return { ...r, ...createMock() } }))
   const mocked = derived(optimistic, O.map((r) => { return { ...r, ...createMock() } }))
   mocked.subscribe(funLog("mocked"))
   const withMocked = derived([notePanel, mocked], ([a, b]) =>
@@ -23,33 +22,21 @@
   )
   withMocked.subscribe(funLog("withMocked"))
 
-  // $: optimistic = pipe(
-  //   optimistic,
-  //   O.chain((mn) =>
-  //     pipe(
-  //       Object.values($notePanel || []),
-  //       A.findFirst((r) => r.snippet_uuid == mn.snippet_uuid),
-  //       O.match(
-  //         () => O.some(mn),
-  //         (a) => O.none,
-  //       ),
-  //     ),
-  //   ),
-  // )
-
-  // if (!(source_id in $note_store)) $note_store = []
   let noteOpens = ($notePanel || []).map((_) => false)
   let closeAll = () => {
     noteOpens = noteOpens.map((_) => false)
   }
-  // $: console.log($note_store, source_id)
 </script>
 
-<!-- {$note_panel.length}<br />{JSON.stringify($curr_source)} -->
-<!-- I had to add || [] here... ofc $note_store wasnt guaranteed to be T[]..., is it time to refactor? -->
-<!-- I definitely shouldn't "just index" and expect it to work -->
 {#each $withMocked as note_data, i}
   <Note note_data={{ ...note_data, searchArt: O.none }} bind:isOpen={noteOpens[i]} {closeAll} {syncLike} />
 {:else}
-  No saves yet...
+  <div class="w-full text-center">
+    No saves yet...<br />
+    <div
+      class="underline tooltip"
+      data-tip="Highlight a piece (e.g. a sentence) of the page and press {altKey} + X">
+      How to add?
+    </div>
+  </div>
 {/each}
