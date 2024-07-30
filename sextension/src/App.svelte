1638be351a583a2e6e27359ccda71fe2cd0c35db
Pawel Paradysz <pa.paradysz@gmail.com>
2024-07-30 11:42:44 +0200

    warning -> info that not logged in

--- sextension/src/App.svelte
+++ sextension/src/App.svelte
@@ -81,7 +81,7 @@
 
 <!-- {$source_id} {session} -->
 {#if O.isNone($session)}
-  <div role="alert" class="alert alert-warning grid-flow-col justify-items-start text-start p-1">
+  <div role="alert" class="alert alert-info grid-flow-col justify-items-start text-start p-1">
     <!-- prettier-ignore -->
     <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
     <span>Not logged in! <a href={login_url} target="_blank" class="underline">link</a></span>
