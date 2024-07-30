e2bf7d5b9abf44905ee3a0e1ff302977101b2885
Pawel Paradysz <pa.paradysz@gmail.com>
2024-07-30 11:22:46 +0200

    delete more commented code

--- shared/src/lib/dashboard/components/TagView.svelte
+++ shared/src/lib/dashboard/components/TagView.svelte
@@ -74,11 +74,8 @@
   const _makeOnlyGroup = (tags: string[]) => {
     $exclTagSets.sets[$exclTagSets.currId] = new Set($allTags)
     _toggleTagGroup(tags)
-    // const shortest = tags.toSorted(asc((x) => x.length))[0] // just in case
-    // doesnt work if base is not a tag!!! so a/b is tag but a not tag
     const group = tags[0].split("/")[0] // top lvl
     if (group !== undefined) location.hash = group || " "
-    // if (shortest !== undefined) location.hash = shortest || " "
   }
   const makeOnly = groupize(_makeOnly, _makeOnlyGroup)
   // let modalPotential: boolean
@@ -88,9 +85,7 @@
   const updateTag = () => {
     if (newTag === "") return false
     if (currTag === newTag) return true
-    // noteDeri.sync.tagUpdate(currTag, O.some(newTag))
     noteDeri.sync.tagUpdate(currTag, O.some(newTag), get(allTags))
-    // noteDeri.sync.t
     currTag = newTag
     return true
   }
