8622fcd9e43fbc954fd608e81a16f05266384973
Pawel Paradysz <pa.paradysz@gmail.com>
2024-08-11 18:33:32 +0200

    yeaa all consistent now

--- supabase-sveltekit/src/routes/account/login/+page.svelte
+++ supabase-sveltekit/src/routes/account/login/+page.svelte
@@ -12,15 +12,18 @@
   const customAppearance = {
     button: "",
   }
-
-  onMount(() =>
+  let darkTheme = true
+  onMount(() => {
     data.supabase.auth.onAuthStateChange(async (event, session) => {
       if (event == "SIGNED_IN") {
         if (session) $sessStore = O.some(session)
-        setTimeout(() => /\/account\//.test(get(page).url.pathname) && goto("/dashboard?from=login"), 50)
+        setTimeout(() => /\/account\//.test(get(page).url.pathname) && goto("/dashboard?from=login"), 50) // TODO: here should scroll down
       }
-    }),
-  )
+    })
+    // TODO: this is a hack to make the theme work
+    const html = document.querySelector("html")
+    if (html) darkTheme = html.classList.contains("dark")
+  })
   $: supabaseClient = data.supabase
   // if (ThemeSupa.default.colors) {
   //   ThemeSupa.default.colors.brand = ""
@@ -44,18 +47,31 @@
         theme: ThemeSupa,
         style: {
           button: "border-radius: 20px",
-          input: "border-radius: 20px; ",
-          //..
+          input: "border-radius: 20px",
+        },
+        variables: {
+          default: {
+            colors: {
+              brand: "oklch(var(--p))",
+              brandAccent: "oklch(var(--ac))",
+              inputText: "oklch(var(--bc))",
+              brandButtonText: "oklch(var(--pc))",
+              // messageText: "oklch(var(--b))",
+              dividerBackground: "oklch(var(--n))",
+              // inputLabelText: "oklch(var(--n))",
+              defaultButtonText: "oklch(var(--n))",
+              anchorTextColor: "oklch(var(--p))",
+            },
+          },
         },
       }} />
-    <div class=" w-full text-right py-1 pr-1">
-      <a
-        href="/account/login/forgotten-password"
-        class="dark:text-orange-300 text-black text-sm hover:underline">Forgotten password</a>
+    <div class="w-full text-right py-1 pr-1">
+      <a href="/account/login/forgotten-password" class="dark:text-yellow-100 text-sm hover:underline"
+        >Forgotten password</a>
     </div>
-    <div class="dark:text-white text-center text-sm p-3">
+    <div class="text-center text-sm p-3">
       Do not have an account?<br />
-      <a href="/account/login/sign-up" class=" text-black dark:text-orange-300 w-full text-md hover:underline"
+      <a href="/account/login/sign-up" class=" dark:text-yellow-100 w-full text-base hover:underline"
         >Sign Up</a>
     </div>
     <br />
