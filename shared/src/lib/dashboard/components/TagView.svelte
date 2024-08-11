95938395a1486d991b406d7e18c0d7021f46ab3b
Pawel Paradysz <pa.paradysz@gmail.com>
2024-08-11 09:26:40 +0200

    lil cleanup

--- shared/src/lib/dashboard/components/TagView.svelte
+++ shared/src/lib/dashboard/components/TagView.svelte
@@ -126,9 +126,7 @@
   // const twoPlusTags = writable(false)
 </script>
 
-<div
-  class="sticky top-0 z-20 overflow-y-scroll overflow-x-visible h-screen no-scrollbar min-w-24 lg:ml-4"
-  bind:this={div}>
+<div class="sticky top-0 z-20 overflow-y-scroll h-screen no-scrollbar min-w-24 lg:ml-4" bind:this={div}>
   <!-- <button  TODO: on phones it'd be nice for it to be collapsible the way left bar is
     class="btn btn-sm btn-square btn-secondary opacity-70 hover:opacity-100 absolute right-0 z-10 rounded-s-btn rounded-e-none"
     on:click={() => (moreTags = !moreTags)}>
