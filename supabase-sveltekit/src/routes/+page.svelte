729347107907a2b97159dbf5018664cba1269667
Pawel Paradysz <pa.paradysz@gmail.com>
2024-09-14 11:22:38 +0200

    landing page update

--- supabase-sveltekit/src/routes/+page.svelte
+++ supabase-sveltekit/src/routes/+page.svelte
@@ -8,6 +8,8 @@
   import IconClipboard from "~icons/jam/clipboard"
   import IconSearch from "~icons/jam/search"
   import { option as O } from "fp-ts"
+  import bookmarkExport from "$lib/assets/bookmark export.png"
+
   export let data
   const { supabase } = data
   onMount(async () => {
@@ -159,11 +161,114 @@
       </div>
     </div>
     <!-- End Icon Block -->
-
-    <!-- End Icon Block -->
   </div>
 </div>
 <!-- End Icon Blocks -->
+<!-- Features -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <!-- Grid -->
+  <!-- <div class="collapse">
+    <input type="checkbox" />
+    <div class="collapse-title text-xl font-medium text-center">What about my data? If I quit?</div>
+    <div class="collapse-content"> -->
+  <div class="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
+    <div>
+      <img class="rounded-xl" src={bookmarkExport} alt="Bookmark export" />
+    </div>
+    <!-- End Col -->
+
+    <div class="mt-5 sm:mt-10 lg:mt-0">
+      <div class="space-y-6 sm:space-y-8">
+        <!-- Title -->
+        <div class="space-y-2 md:space-y-4">
+          <h2 class="font-bold text-2xl md:text-3xl lg:text-4xl text-gray-800 dark:text-neutral-200">
+            Worried what happens to your data when you quit?
+          </h2>
+          <p class="text-gray-500 dark:text-neutral-500">
+            You can export saves to bookmarks to keep everything.
+          </p>
+        </div>
+        <!-- End Title -->
+
+        <!-- List -->
+        <ul class="space-y-2 sm:space-y-4">
+          <li class="flex gap-x-3">
+            <span
+              class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg
+                class="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"><polyline points="20 6 9 17 4 12" /></svg>
+            </span>
+            <div class="grow">
+              <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                Highlights are added to bookmark titles
+              </span>
+            </div>
+          </li>
+
+          <li class="flex gap-x-3">
+            <span
+              class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg
+                class="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"><polyline points="20 6 9 17 4 12" /></svg>
+            </span>
+            <div class="grow">
+              <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                Tags turn into folders
+              </span>
+            </div>
+          </li>
+
+          <li class="flex gap-x-3">
+            <span
+              class="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
+              <svg
+                class="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"><polyline points="20 6 9 17 4 12" /></svg>
+            </span>
+            <div class="grow">
+              <span class="text-sm sm:text-base text-gray-500 dark:text-neutral-500">
+                Text search will work in the browser's URL bar
+              </span>
+            </div>
+          </li>
+        </ul>
+        <!-- End List -->
+      </div>
+    </div>
+    <!-- End Col -->
+  </div>
+  <!-- </div>
+  </div> -->
+
+  <!-- End Grid -->
+</div>
+<!-- End Features -->
 <div class="flex justify-center items-center h-min-[30rem] w-full mt-20 pb-32 relative">
   <div
     class="text-5xl font-bold mt-3 pt-3 top-0 left-1/2 absolute transform -translate-x-1/2 -translate-y-1/2">
