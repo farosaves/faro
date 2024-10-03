dc997e80eaa5f76a4596ac0b6788580971a63f5e
Pawel Paradysz <pa.paradysz@gmail.com>
2024-10-03 07:22:43 +0200

    .

--- new-ext/wxt.config.ts
+++ new-ext/wxt.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "wxt"
-import path from "path"
-
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
-  alias: { $lib: path.resolve(__dirname, "./src/lib") },
   modules: ["@wxt-dev/module-svelte"],
-  vite: _config => ({}),
 })
