e169c37a9f0fbd2803f5bd67249b00927474d54e
Pawel Paradysz <pa.paradysz@gmail.com>
2024-10-05 19:03:15 +0200

    right click on tag 2 edit

--- supabase-sveltekit/src/routes/dashboard/example/+page.svelte
+++ supabase-sveltekit/src/routes/dashboard/example/+page.svelte
@@ -24,8 +24,14 @@
       await T.multipleNotes.query([
         "4afc1421-9eb2-4532-b2c1-c39c355ad811",
         "64ba8738-2d05-48f6-9395-7019deff2ec4",
+        "85ef627b-66d5-4304-a1b7-bd57b4a1c33f",
+        "fec270c5-ba93-45f1-9c48-93f19f83c59a",
+        "96cf304f-90f8-404b-b5b4-bea4a741a386",
+        "a6a71bdc-0fd5-4a7b-bccc-2d6800909f17",
+        "22c41cc7-2f91-468d-9551-855ace0068c8",
       ])
-    ).data
+    ).data?.map((n) => ({ ...n, prioritised: 0 }))
+
     window.umami.track("example dashboard open")
     if (!notes) return
     mock.notes = new Map(notes.map((n) => [n.id, n]))
