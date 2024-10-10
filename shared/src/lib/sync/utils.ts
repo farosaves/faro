109a7f44c47f0369c049898e5247e642ec2c74d2
Pawel Paradysz <pa.paradysz@gmail.com>
2024-10-10 10:37:21 +0200

    paignation handling 1

--- shared/src/lib/sync/utils.ts
+++ shared/src/lib/sync/utils.ts
@@ -6,7 +6,8 @@ import type { Patch } from "immer"
 
 export const getUpdatedNotes = async (supabase: SupabaseClient, user_id: string, lastDate: string) => {
   const { data } = await supabase.from("notes").select("*").eq("user_id", user_id).gt("updated_at", lastDate)
-    .order("updated_at", { ascending: true })// .limit(1000)  // TODO: pagination
+    .order("updated_at", { ascending: true }).limit(10000) // TODO: pagination
+    // I think the pagination should be handled here and not in the fetch new notes function.
   return data || []
 }
 
