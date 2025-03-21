df2abba560e5d72efe480f2b4957eb48ceb2adc4
Pawel Paradysz <pa.paradysz@gmail.com>
2025-03-21 10:50:02 +0100

    testing

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.{test,spec}.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/playwright-report/**', '**/tests/**'],
+    environment: 'jsdom',
+  },
+}); 
